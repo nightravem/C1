e212a1690c5580457b5a7104200369b66dad8ae5
night <nightravem@gmail.com>
2022-02-20 02:26:12 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+fn C1_f1(a : i32) {
+    println!("call funtion in C1, function name C1_f1, args: {}", a);
+}
